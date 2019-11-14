63323469803924e2d1a4fa2dca0b0987fbe2011b
Regan Laurell <reganlaurell@gmail.com>
2019-11-14 09:08:52 -0500

    add method to check if all fields are filled in

--- CreateNewUser.aspx.cs
+++ CreateNewUser.aspx.cs
@@ -11,4 +11,40 @@ public partial class CreateNewUser : System.Web.UI.Page
     {
 
     }
+    
+    protected void CreateUser_Click(object sender, EventArgs e)
+    {
+        string firstName = firstNameTextBox.Text;
+        string lastName = lastNameTextBox.Text;
+        string email = emailTextBox.Text;
+        string phoneNumber = phoneNumberTextBox.Text;
+        string jobTitle = jobTitleTextBox.Text;
+        string company = companyNameTextBox.Text;
+        string username = usernameTextBox.Text;
+        string password = passwordTextBox.Text;
+
+        if(allFieldsFilledIn(lastName, email, phoneNumber, company, username, password))
+        {
+
+        }
+    }
+
+    private bool allFieldsFilledIn(string name, string email, string phoneNumber, string company, string username, string password)
+    {
+        if(email.Length > 0|| phoneNumber.Length > 0)
+        {
+            if(name.Length > 0 && company.Length > 0 && username.Length > 0 && password.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
