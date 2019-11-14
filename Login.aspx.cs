a62a7b5b408e1bc2df67454305fd1dd2206f8908
Regan Laurell <reganlaurell@gmail.com>
2019-11-14 08:49:37 -0500

    add redirect to create page on button click

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -14,6 +14,6 @@ public partial class Login : System.Web.UI.Page
 
     protected void createButton_Click(object sender, EventArgs e)
     {
-
+        Response.Redirect("~/CreateNewUser.aspx");
     }
 }
