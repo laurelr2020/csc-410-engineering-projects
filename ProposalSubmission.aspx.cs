1a5c683750e02d31abc5ec2e378307ab14f064a8
Regan Laurell <reganlaurell@gmail.com>
2019-11-19 08:14:47 -0500

    begin insert method

--- ProposalSubmission.aspx.cs
+++ ProposalSubmission.aspx.cs
@@ -11,4 +11,59 @@ public partial class ProposalSubmission : System.Web.UI.Page
     {
 
     }
+
+    protected void ProposalSubmission_Click(object sender, EventArgs e)
+    {
+
+
+        if (allFieldsFilledIn())
+        {
+
+            try
+            {
+                statusLabel.Text = "Proposal was added successfully";
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = ex.Message;
+            }
+        }
+        else
+        {
+            statusLabel.Text = "Please enter information for all fields";
+        }
+
+        firstNameTextBox.Text = "";
+        lastNameTextBox.Text = "";
+        emailTextBox.Text = "";
+        phoneNumberTextBox.Text = "";
+        jobTitleTextBox.Text = "";
+        companyNameTextBox.Text = "";
+        usernameTextBox.Text = "";
+        passwordTextBox.Text = "";
+    }
+
+    private string replaceSingleQuote(string dataField)
+    {
+        return dataField.Replace("'", "");
+    }
+
+    private bool allFieldsFilledIn()
+    {
+        if (email.Length > 0 || phoneNumber.Length > 0)
+        {
+            if (name.Length > 0 && company.Length > 0 && username.Length > 0 && password.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
